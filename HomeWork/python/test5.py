be13861571e86c9d166cf9fc7075c4a16cbb9a2b
loveandevol <1403326337@qq.com>
2021-04-06 17:35:14 +0800

    python homework

---
+++ HomeWork/python/test5.py
@@ -0,0 +1,32 @@
+import numpy as np
+#1.创建两个数组arr1 = [1,2,3,4]和arr2 = [5,6,7,8]，对这两个数组进行四则运算。\
+print("四则运算----------->>>")
+arr1 = np.array([1,2,3,4])
+arr2 = np.array([5,6,7,8])
+print(arr1+arr2)
+print(arr1-arr2)
+print(arr1*arr2)
+print(arr1/arr2)
+#2.创建数组arr3 =［［1, 2, 3, 4］,［4, 5, 6, 7］, ［7, 8, 9, 10］］，将数据数组arr3的形状改为（4,3）并输出。
+print("reshape----------->>>")
+arr3 = np.array([[1,2,3,4],[4,5,6,7],[7,8,9,10]])
+print(arr3.reshape(4,3))
+#3.创建两个形状相同的数据，并将这两数组分别进行水平叠放、垂直叠放和深度叠放。
+print("叠放----------->>>")
+arr1 = np.arange(12).reshape(4,3)
+arr2 = np.arange(12).reshape(4,3)
+print(np.hstack((arr1,arr2)))
+print(np.vstack((arr1,arr2)))
+print(np.dstack((arr1,arr2)))
+#4.创建数组arr9 = [1,1,5,7,2,8,4]，分别完成排序、去重、总和、均值、标准差、方差、最小值、最大值的统计。
+print("统计运算----------->>>")
+arr9 = np.array([1,1,5,7,2,8,4])
+arr9.sort()
+print(arr9)
+print(np.unique(arr9))
+print(np.sum(arr9))
+print(np.mean(arr9))
+print(np.std(arr9))
+print(np.var(arr9))
+print(np.min(arr9))
+print(np.max(arr9))
