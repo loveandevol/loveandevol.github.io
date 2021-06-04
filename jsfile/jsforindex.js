8889fa24e5c626a2dd7a30d4b42b431bb4ea7ab3
ceelia0 <ceelia0@163.com>
2021-06-04 18:46:20 +0800

    update

--- jsfile/jsforindex.js
+++
@@ -1,8 +0,0 @@
-document.write("<h1>unjust laws exist;</h1>");
-document.write("<h1>shall we be content to obey them,</h1>");
-document.write("<h1>and obey them until we have succeeded,</h1>");
-document.write("<h1>or shall we transgress them at once?</h1>");
-document.write("<h3>--Henry David Thoreau</h3>");
-d = new Date();
-document.write(d.toString());
-document.write("\u00a9");
