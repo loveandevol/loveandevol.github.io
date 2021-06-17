d10179d3ade8ba5691b5a8b883ecfca5b6aa6e18
ceelia0 <ceelia0@163.com>
2021-06-17 23:51:10 +0800

    new contents

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ *  file: index.js
+ *  June 6,2021
+ *  writen by Mr.Cheng
+ *
+ */
+
+function ChangeWords(id) {
+  id.innerHTML="CEE-BLOG";
+}
+
+console.log(document.getElementsByClassName("topnav"));
